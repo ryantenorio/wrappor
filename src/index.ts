57b1ecea28019294a07cdece6eb5ee35c1fda889
ryantenorio <ryantenorio@gmail.com>
2021-02-16 21:01:12 -0800

    encapsulate calls into a class with encode API

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,19 @@ interface Encoder {
   randGenerator: RRandom
 }
 
-type RapporMode = 'STANDARD' | 'ONE-TIME' | 'BASIC' | 'BASIC ONE-TIME'
+export type RapporMode = 'STANDARD' | 'ONE-TIME' | 'BASIC' | 'BASIC ONE-TIME'
 
-export class WrapporEncoder implements Encoder {
+export const bigEndianOf = (val: number, bytes: number) => {
+  // tried with a dataview, ended up using this solution: https://github.com/willscott/rappor/blob/master/rappor.js#L144
+  let result = "";
+  for (let i = (bytes - 1) * 8; i >= 0; i -= 8) {
+    let currByte = (val & (0xFF << i)) >> i
+    result = result.concat(String.fromCharCode(currByte))
+  }  
+  return result
+}
+
+export class Wrappor implements Encoder {
   config: EncoderConfig;
   clientCohort: number;
   clientSecret: string;
@@ -39,7 +49,7 @@ export class WrapporEncoder implements Encoder {
     this.rapporMode = mode
   }
 
-  signal = (v: string, cohort: number, numHashes: number, bloomSize: number) => {
+  private signal = (v: string, cohort: number, numHashes: number, bloomSize: number) => {
     // encode the value with the cohort the same way Google does it for rappor.py 
     let value = bigEndianOf(cohort, 4) + v
   
@@ -55,101 +65,72 @@ export class WrapporEncoder implements Encoder {
   
     return bloomBits
   }
-}
 
-export const bigEndianOf = (val: number, bytes: number) => {
-  // tried with a dataview, ended up using this solution: https://github.com/willscott/rappor/blob/master/rappor.js#L144
-  let result = "";
-  for (let i = (bytes - 1) * 8; i >= 0; i -= 8) {
-    let currByte = (val & (0xFF << i)) >> i
-    result = result.concat(String.fromCharCode(currByte))
-  }  
-  return result
-}
-
-// hash client's value v onto bloom filter B of size k using h hash functions
-// returns the list of bits to activate in B
-export const signal = (v: string, cohort: number, numHashes: number, bloomSize: number) => {
-  // encode the value with the cohort the same way Google does it for rappor.py 
-  let value = bigEndianOf(cohort, 4) + v
-
-  // hashing
-  let hash = md5(value, {"asString": true})
+  private buildBloom = (bitsToActivate: number[]) => {
+    let bloom = 0
+    bitsToActivate.forEach(bit => {
+      bloom |= (1 << bit)
+    })
+    return bloom
+  }
   
-  let bloomBits: number[]
-  bloomBits = []
-  // rappor.py uses xrange which is still 0-indexed. TIL!
-  for(let i = 0; i < numHashes; i++) {
-    bloomBits.push(hash.charCodeAt(i) % bloomSize)
+  private doSignal = (v: string, cohort: number, numHashes: number, bloomSize: number) => {
+    let bloomBits = this.signal(v, cohort, numHashes, bloomSize)
+    let bloom = this.buildBloom(bloomBits)
+    return bloom
   }
-
-  return bloomBits
-}
-
-export const buildBloom = (bitsToActivate: number[]) => {
-  let bloom = 0
-  bitsToActivate.forEach(bit => {
-    bloom |= (1 << bit)
-  })
-  return bloom
-}
-
-export const doSignal = (v: string, cohort: number, numHashes: number, bloomSize: number) => {
-  let bloomBits = signal(v, cohort, numHashes, bloomSize)
-  let bloom = buildBloom(bloomBits)
-  return bloom
-}
-
-export const getPRRMasks = (word: string, secret: string, f: number, numBits: number) => {
-  let hash = HmacSHA256(word, secret).toString()
-
-  let threshold128 = f * 128
-  let uniform = 0
-  let fMask = 0
-
-  // hex string to byte array from crypto-js: https://stackoverflow.com/a/34356351
-  let hashBytes = []
-  for(let c = 0; c < hash.length; c += 2) {
-    hashBytes.push(parseInt(hash.substr(c, 2), 16))
+  
+  private getPRRMasks = (word: string, secret: string, f: number, numBits: number) => {
+    let hash = HmacSHA256(word, secret).toString()
+  
+    let threshold128 = f * 128
+    let uniform = 0
+    let fMask = 0
+  
+    // hex string to byte array from crypto-js: https://stackoverflow.com/a/34356351
+    let hashBytes = []
+    for(let c = 0; c < hash.length; c += 2) {
+      hashBytes.push(parseInt(hash.substr(c, 2), 16))
+    }
+  
+    for(let i = 0; i < numBits; i++) {    
+      let byte = hashBytes[i]
+      let uBit = byte & 0x01    
+      
+      uniform |= (uBit << i)
+  
+      let rand128 = byte >> 1 // 7 bits of entropy
+      let noiseBit: number = (rand128 < threshold128) ? 1 : 0        
+      fMask |= (noiseBit << i)
+    }
+    return [uniform, fMask]
   }
-
-  for(let i = 0; i < numBits; i++) {    
-    let byte = hashBytes[i]
-    let uBit = byte & 0x01    
+  
+  private doPRR = (bloom: number, secret: string, f: number, bits: number) => {
+    // index 0 is uniform, 1 is fmask
+    // 16 bloombits, 2 hashes, 54 cohorts
     
-    uniform |= (uBit << i)
-
-    let rand128 = byte >> 1 // 7 bits of entropy
-    let noiseBit: number = (rand128 < threshold128) ? 1 : 0        
-    fMask |= (noiseBit << i)
-  }
-  return [uniform, fMask]
-}
-
-export const doPRR = (bloom: number, secret: string, f: number, bits: number) => {
-  // index 0 is uniform, 1 is fmask
-  // 16 bloombits, 2 hashes, 54 cohorts
+    let masks = this.getPRRMasks(bigEndianOf(bloom, 4), secret, f, bits)  
+    let prr = 0
   
-  let masks = getPRRMasks(bigEndianOf(bloom, 4), secret, f, bits)  
-  let prr = 0
-
-  prr = (bloom & ~masks[1]) | (masks[0] & masks[1])
-
-  return prr
-}
-
-export const doIRR = (prr: number, randProvider: RRandom, p: number, q: number, s: number) => {
-  let irr = 0
-  let pBits = randProvider.generatePMask(p, s)
-  let qBits = randProvider.generateQMask(q, s)
-  irr = (pBits & ~prr) | (qBits & prr)
-  return irr
-}
-
-export const encode = (word: string, secret: string, cohort: number, hashes: number, bits: number, f: number, p: number, q: number, randProvider: RRandom) => {
-  let irr = 0
-  let bloom = doSignal(word, cohort, hashes, bits)
-  let prr = doPRR(bloom, secret, f, bits)
-  irr = doIRR(prr, randProvider, p, q, bits)
-  return irr
+    prr = (bloom & ~masks[1]) | (masks[0] & masks[1])
+  
+    return prr
+  }
+  
+  private doIRR = (prr: number, randProvider: RRandom, p: number, q: number, s: number) => {
+    let irr = 0
+    let pBits = randProvider.generatePMask(p, s)
+    let qBits = randProvider.generateQMask(q, s)
+    irr = (pBits & ~prr) | (qBits & prr)
+    return irr
+  }
+  
+  encode = (word: string) => {
+    let irr = 0
+    let bloom = this.doSignal(word, this.clientCohort, this.config.hashes, this.config.bloomBits)
+    let prr = this.doPRR(bloom, this.clientSecret, this.config.fProb, this.config.bloomBits)
+    irr = this.doIRR(prr, this.randGenerator, this.config.pProb, this.config.qProb, this.config.bloomBits)
+    return irr
+  }
 }
--- test/wrappor.test.ts
+++ test/wrappor.test.ts
@@ -1,82 +1,4 @@
-import { bigEndianOf, buildBloom, doIRR, doPRR, encode, getPRRMasks, RRandom, signal } from '../src';
-
-describe('Big Endian', () => {
-  it('returns correct representation', () => {
-    let testValue = 8256,
-    numBytes = 4,
-    expectedVal = "\x00\x00 @"
-
-    expect(bigEndianOf(testValue, numBytes)).toEqual(expectedVal)
-  })
-})
-
-describe('Signal Step', () => {
-  it('identifies correct bloom bits to set', () => {
-    let cohort = 0,
-    hashes = 2,
-    word = 'abc',
-    bloomSize = 16,
-    expected = [6, 13]
-
-    expect(signal(word, cohort, hashes, bloomSize)).toEqual(expected)
-  })
-
-  it('applies bloom bits to the bloom filter', () => {
-    let bits = [6, 13]
-    let expected = 8256
-    expect(buildBloom(bits)).toEqual(expected)
-  })
-})
-
-describe('PRR Step', () => {
-  it('calculates uniform and fmask correctly', () => {
-    let word = 'v3'
-    let secret = 'secret'
-    let f = 0.5
-    let numBits = 8
-    let expected = [150, 202]
-    expect(getPRRMasks(word, secret, f, numBits)).toEqual(expected)
-  })
-
-  it('generates the PRR correctly', () => {
-    let bloom = 8256 // the string abc
-    let secret = 'secret'
-    let f = 0.5
-    let numBits = 16
-    let expected = 57576
-    expect(doPRR(bloom, secret, f, numBits)).toEqual(expected)
-  })
-})
-
-describe('IRR Step', () => {
-  it('generates the IRR correctly', () => {
-    const prr = 57576
-    const p = 0.5
-    const numBits = 16
-    const q = 0.75
-    const seeds = [0.0, 0.6, 0.0]
-    const mockRRandom = new MockRRandom(seeds)
-    const expected = 64493
-    expect(doIRR(prr, mockRRandom, p, q, numBits)).toEqual(expected)
-  })
-})
-
-describe('Full Encode', () => {
-  it('generates IRR correctly', () => {
-    const word = 'abc'
-    const secret = 'secret'
-    const cohort = 0
-    const hashes = 2
-    const bits = 16
-    const f = 0.5
-    const p = 0.5
-    const q = 0.75
-    const seeds = [0.0, 0.6, 0.0]
-    const mockRRandom = new MockRRandom(seeds)
-    const expected = 64493
-    expect(encode(word, secret, cohort, hashes, bits, f, p, q, mockRRandom)).toEqual(expected)
-  })
-})
+import { bigEndianOf, EncoderConfig, RapporMode, RRandom, Wrappor } from '../src';
 
 class MockRRandom implements RRandom {
   seeds: number[]  
@@ -102,3 +24,96 @@ class MockRRandom implements RRandom {
     return r
   }
 }
+
+describe('Big Endian', () => {
+  it('returns correct representation', () => {
+    let testValue = 8256,
+    numBytes = 4,
+    expectedVal = "\x00\x00 @"
+
+    expect(bigEndianOf(testValue, numBytes)).toEqual(expectedVal)
+  })
+})
+
+describe('Wrappor', () => {
+  let seeds: number[] = [0.0, 0.6, 0.0],  
+  randGenerator: RRandom = new MockRRandom(seeds),
+  mode: RapporMode = 'STANDARD',
+  clientSecret: string = 'secret',
+  clientCohort: number = 0,
+  config: EncoderConfig = {
+    bloomBits: 16,
+    hashes: 2,
+    totalCohorts: 64,
+    pProb: 0.5,
+    qProb: 0.75,
+    fProb: 0.5
+  }
+
+  let testObj = new Wrappor(config, clientCohort, clientSecret, randGenerator, mode)
+
+  // this is the test pass that really matters
+  it('full encoding pass works correctly', () => {
+    expect(testObj.encode('abc')).toEqual(64493)
+  })
+
+  // the following unit tests use ts-ignore so we can test the private functions without complaint
+  // unless a use-case would be discovered, the full encode is the only API that should really be exposed
+  describe('Signal Step', () => {
+    it('identifies correct bloom bits to set', () => {
+      let cohort = 0,
+      hashes = 2,
+      word = 'abc',
+      bloomSize = 16,
+      expected = [6, 13]      
+      // @ts-ignore
+      expect(testObj.signal(word, cohort, hashes, bloomSize)).toEqual(expected)
+    })
+  
+    it('applies bloom bits to the bloom filter', () => {
+      let bits = [6, 13]
+      let expected = 8256
+      // @ts-ignore
+      expect(testObj.buildBloom(bits)).toEqual(expected)
+    })
+  })
+  
+  describe('PRR Step', () => {
+    it('calculates uniform and fmask correctly', () => {
+      let word = 'v3'
+      let secret = 'secret'
+      let f = 0.5
+      let numBits = 8
+      let expected = [150, 202]
+      // @ts-ignore
+      expect(testObj.getPRRMasks(word, secret, f, numBits)).toEqual(expected)
+    })
+  
+    it('generates the PRR correctly', () => {
+      let bloom = 8256 // the string abc
+      let secret = 'secret'
+      let f = 0.5
+      let numBits = 16
+      let expected = 57576
+      // @ts-ignore
+      expect(testObj.doPRR(bloom, secret, f, numBits)).toEqual(expected)
+    })
+  })
+  
+  describe('IRR Step', () => {
+    it('generates the IRR correctly', () => {
+      const prr = 57576
+      const p = 0.5
+      const numBits = 16
+      const q = 0.75
+      const seeds = [0.0, 0.6, 0.0]
+      const mockRRandom = new MockRRandom(seeds)
+      const expected = 64493
+      // @ts-ignore
+      expect(testObj.doIRR(prr, mockRRandom, p, q, numBits)).toEqual(expected)
+    })
+  })
+})
+
+
+
